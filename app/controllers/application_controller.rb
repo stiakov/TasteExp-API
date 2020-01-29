6be29fe090c65687e3464835eb38814a24e383ef
Santiago Torres G <thdealer@gmail.com>
2020-01-29 18:07:33 -0500

    adding permitted params to user register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
   respond_to :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+  end
 end
