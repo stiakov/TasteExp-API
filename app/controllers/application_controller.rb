16bd93558b15c2488dd8eddd84a8c791a232d750
Santiago Torres G <thdealer@gmail.com>
2020-02-03 13:34:56 -0500

    adding allowed params to user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
   end
 end
