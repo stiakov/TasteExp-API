1c7f01a97dc3d51c58e2a358aee6ddd3a12cc36d
Santiago Torres G <thdealer@gmail.com>
2020-02-13 18:28:58 -0500

    Update validation in user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:commerces).dependent(:destroy) }
   it { should have_many(:photos).dependent(:destroy) }
   it { should have_many(:reservations).dependent(:destroy) }
   it { should have_many(:favorites).dependent(:destroy) }
