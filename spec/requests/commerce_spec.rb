f44e720ef610aa186b020487aadfd486b7d1569c
Santiago Torres G <thdealer@gmail.com>
2020-02-14 12:53:15 -0500

    Update request specs for Commerces

--- spec/requests/commerce_spec.rb
+++ spec/requests/commerce_spec.rb
@@ -12,37 +12,70 @@ RSpec.describe 'Commerces', type: :request do
     end
   end
 
-  context 'restricted actions for non-authenticated user' do
-    it 'commerce create returns status 401' do
+  context 'restricted actions for NON_AUTHENTICATED user' do
+    it 'Commerce create returns status 401' do
       post v1_commerces_create_path
       expect(response).to have_http_status(401)
     end
 
-    it 'commerce create returns a body that asks for signin or signup' do
+    it 'Commerce create returns a body that asks for SignIn or SignUp' do
       post v1_commerces_create_path
-      error = {"errors":['You need to sign in or sign up before continuing.']}
+      error = { errors: ['You need to sign in or sign up before continuing.'] }
       expect(response.body).to include(error.to_json)
     end
 
-    it 'commerce update returns status 401' do
+    it 'Commerce update returns status 401' do
       put v1_commerces_update_path(1)
       expect(response).to have_http_status(401)
     end
 
-    it 'commerce update returns a body that asks for signin or signup' do
+    it 'Commerce update returns a body that asks for SignIn or SignUp' do
       put v1_commerces_update_path(1)
-      error = {"errors":['You need to sign in or sign up before continuing.']}
+      error = { errors:['You need to sign in or sign up before continuing.'] }
       expect(response.body).to include(error.to_json)
     end
 
-    it 'commerce delete returns status 401' do
+    it 'Commerce delete returns status 401' do
       delete v1_commerces_destroy_path(1)
       expect(response).to have_http_status(401)
     end
 
-    it 'commerce delete returns a body that asks for signin or signup' do
+    it 'Commerce delete returns a body that asks for SignIn or SignUp' do
       delete v1_commerces_destroy_path(1)
-      error = {"errors":['You need to sign in or sign up before continuing.']}
+      error = { errors:['You need to sign in or sign up before continuing.'] }
+      expect(response.body).to include(error.to_json)
+    end
+
+    it 'Commerce Not Saved action returns status 401' do
+      get v1_commerces_notsaved_path
+      expect(response).to have_http_status(401)
+    end
+
+    it 'Commerce Not Saved action returns a body that asks for SignIn or SignUp' do
+      get v1_commerces_notsaved_path
+      error = { errors:['You need to sign in or sign up before continuing.'] }
+      expect(response.body).to include(error.to_json)
+    end
+
+    it 'Commerce Filtering  Not Saved action returns status 401' do
+      get v1_commerces_not_saved_filter_path(1)
+      expect(response).to have_http_status(401)
+    end
+
+    it 'Commerce Filtering  Not Saved action returns a body that asks for SignIn or SignUp' do
+      get v1_commerces_not_saved_filter_path(1)
+      error = { errors:['You need to sign in or sign up before continuing.'] }
+      expect(response.body).to include(error.to_json)
+    end
+
+    it 'Commerce Filtering Favorites action returns status 401' do
+      get v1_commerces_not_saved_filter_path(1)
+      expect(response).to have_http_status(401)
+    end
+
+    it 'Commerce Filtering Favorites action returns a body that asks for SignIn or SignUp' do
+      get v1_commerces_favorites_filter_path(1)
+      error = { errors:['You need to sign in or sign up before continuing.'] }
       expect(response.body).to include(error.to_json)
     end
   end
